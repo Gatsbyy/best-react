338d46f615711d94f1c4627514ce0c871247608b
gatsby <784007667@qq.com>
2021-02-03 15:57:54 +0800

    feat: 添加环境配置

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+/**
+ * 配置文件
+ */
+module.exports = {
+  dev: {
+    // 端口
+    port: 5000,
+    // 连接后端API的URL
+    // apiUrl: 'http://47.56.207.189:8399/'
+    apiUrl: 'https://oklinkbeta.bafang.com/'
+  }
+};
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -6,12 +6,11 @@ const webpack = require('webpack');
 const fs = require('fs');
 const env = require('./dev.env');
 const base = require('./webpack.config.base');
-const config = require('./config');
+const config = require('./dev');
 const ip = require('ip');
 
 const address = ip.address();
 
-const mockUrl = 'http://mock.okcoin-inc.com';
 
 base.output.publicPath = `http://${address}:${config.dev.port}/`;
 
@@ -27,44 +26,12 @@ module.exports = Object.assign(base, {
     hot: true,
     host: address,
     port: config.dev.port,
-    before(app) {
-      app.all('/mock/api/tracker/v1/*', (req, res) => {
-        const filename = (req.params[0] || '').split('/').pop();
-
-        const filepath = path.resolve(__dirname, '../src/mock', `${filename}.json`);
-        try {
-          const content = fs.readFileSync(filepath, 'utf-8');
-          const data = JSON.parse(content);
-          const { timeout } = data;
-          setTimeout(() => {
-            res.json(data);
-          }, timeout || 0);
-        } catch (err) {
-          console.error('your mock data filename should be', `${filename}.json`);
-          res.json({
-            code: 1,
-            msg: err.message
-          });
-        }
-      });
-    },
     proxy: {
-      '/rap/*': {
-        target: mockUrl,
-        changeOrigin: true,
-        secure: true,
-      },
       '/api/*': {
         target: config.dev.apiUrl,
         changeOrigin: true,
         secure: true,
       },
-      '/cdn/*': {
-        target: 'https://static.bafang.com',
-        changeOrigin: true,
-        secure: true,
-      },
-
     },
     historyApiFallback: {
       rewrites: [
