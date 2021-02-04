4d6a0ab642edce349c9fce766b03542c7f9a8fa1
gatsby <784007667@qq.com>
2021-02-04 17:57:02 +0800

    feat: 完善eslint配置, hook demo

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 function About() {
+  const [count, setCount] = useState(0);
+
+  setTimeout(() => {
+    setCount(count + 1);
+  }, 1000);
+
   return (
-    <div>About</div>
+    <div>{count}</div>
   )
 }
 
